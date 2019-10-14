b9cfe55555107fb1d305270bc2d6a3c9d7920a6f
Lucas Swartsenburg <lucas@itso.io>
2019-10-14 10:29:55 -0500

    First api step

--- server/src/views/syncs.py
+++ server/src/views/syncs.py
@@ -3,6 +3,7 @@ import time
 from flask import Blueprint, session, request
 from utils.app import jsonify, app
 from utils.tasks import create_task
+from data.api_credentials import get_calendar_api_client
 
 
 syncs = Blueprint('syncs', __name__)
@@ -28,13 +29,15 @@ def list_syncs():
 @syncs.route('/api/syncs', methods=['POST'])
 def configure_sync():
     data = request.json
+    email = session['user_email'] if 'user_email' in session else 'lucas@itso.io'
     print(data)
-    database_id = data['database_id']
     sync_type = data['sync_type'] or 'google_calendar'
     synced_ids = data['synced_ids']
 
     create_task(request, CALENDAR_SYNC_HANDLER_URL, {
-        "food": "burger"
+        "food": "burger",
+        "email": email,
+        "calendars": synced_ids
     })
     return jsonify(data)
 
@@ -42,7 +45,26 @@ def configure_sync():
 @syncs.route(CALENDAR_SYNC_HANDLER_URL, methods=['POST'])
 def task_calendar_sync():
     print("Body:")
-    print(request.data)
+    data = request.json
+    print(data)
+    email = data['email']
+    calendars = [email]  # data['calendars']
+
+    cal_client = get_calendar_api_client(email)
+    print('et')
+
+    for cal in calendars:
+        events_result = cal_client.events().list(calendarId=cal,
+                                            maxResults=10, singleEvents=True,
+                                            orderBy='startTime').execute()
+        events = events_result.get('items', [])
+
+        if not events:
+            print('No upcoming events found.')
+        for event in events:
+            start = event['start'].get('dateTime', event['start'].get('date'))
+            print(start, event['summary'])
+
 
     print("Start sleep")
     time.sleep(5)
