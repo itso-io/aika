2f81243052f293b31865b561e75cccafb4b8038c
Lucas Swartsenburg <lucas@itso.io>
2019-10-23 08:44:58 -0500

    Getting it working locally

--- server/run_local.sh
+++ server/run_local.sh
@@ -3,6 +3,8 @@
 export FLASK_ENV=development
 export OAUTHLIB_INSECURE_TRANSPORT=1
 
+docker run -d -p 3005:3000 --name metabase metabase/metabase
+
 # default behavior of DATASTORE_EMULATOR_HOST being set to values like `::1:8971` doesn't seem to work, so rewrite it.
 $(gcloud beta emulators datastore env-init)
 port=${DATASTORE_EMULATOR_HOST##*:}
