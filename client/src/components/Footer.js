9c08c88ac9917c17578475188bd9e584e2f65d95
Jon Gaulding <jon@itso.io>
2019-10-25 14:48:27 -0500

    Fix Trello link

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -8,7 +8,7 @@ class Footer extends React.Component {
       <div style={{width: '100%', display: 'flex', flexDirection: 'column', alignItems: 'center', position: 'relative', bottom: 0}}>
         <Container maxWidth="md" justify="center" style={{  }}>
           <p>
-            We <span role="img" aria-label="love love LOVE">&#10084;&#65039; &#10084;&#65039; &#10084;&#65039;</span> feedback and feature requests. Email us at <a href="mailto:help@getaika.com">help@getaika.com</a>, or add requests <a href="https://trello.com/b/KR2vqVGw/aika-product-requests" target="_blank" rel="noopener noreferrer">here</a>.
+            We <span role="img" aria-label="love love LOVE">&#10084;&#65039; &#10084;&#65039; &#10084;&#65039;</span> feedback and feature requests. Email us at <a href="mailto:help@getaika.com">help@getaika.com</a>, or add requests <a href="https://trello.com/invite/b/KR2vqVGw/9154fa50b4f3800263fac8e4f133af4b/aika-product-requests" target="_blank" rel="noopener noreferrer">here</a>.
           </p>
         </Container>
       </div>
