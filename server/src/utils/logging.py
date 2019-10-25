ecc2fb1f9c6c25505a06d09bf1790c13746af77e
Lucas Swartsenburg <lucas@itso.io>
2019-10-25 12:26:30 -0500

    Change logging

--- server/src/utils/logging.py
+++ server/src/utils/logging.py
@@ -10,19 +10,19 @@ from logging.config import dictConfig
 
 
 def set_logging(app):
-  dictConfig({
-      'version': 1,
-      'formatters': {'default': {
-          'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
-      }},
-      'handlers': {'wsgi': {
-          'class': 'logging.StreamHandler',
-          'stream': 'ext://flask.logging.wsgi_errors_stream',
-          'formatter': 'default'
-      }},
-      'root': {
-          'level': 'INFO' if app.config['DEBUG'] else 'WARNING',
-          'handlers': ['wsgi']
-      }
-  })
+#   dictConfig({
+#       'version': 1,
+#       'formatters': {'default': {
+#           'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+#       }},
+#       'handlers': {'wsgi': {
+#           'class': 'logging.StreamHandler',
+#           'stream': 'ext://flask.logging.wsgi_errors_stream',
+#           'formatter': 'default'
+#       }},
+#       'root': {
+#           'level': 'INFO' if app.config['DEBUG'] else 'WARNING',
+#           'handlers': ['wsgi']
+#       }
+#   })
   logging.info('Log information loaded')
