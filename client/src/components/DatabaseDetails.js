6012d8e2208f8cfb98376b96b83fa81884629c22
Jon Gaulding <jon@itso.io>
2019-11-15 17:54:14 -0600

    Update front page

--- client/src/components/DatabaseDetails.js
+++ client/src/components/DatabaseDetails.js
@@ -7,15 +7,9 @@ import Button from '@material-ui/core/Button';
 import FormControl from '@material-ui/core/FormControl';
 import FormGroup from '@material-ui/core/FormGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableRow from '@material-ui/core/TableRow';
-import Visibility from '@material-ui/icons/Visibility';
-import VisibilityOff from '@material-ui/icons/VisibilityOff';
-import ReactTooltip from 'react-tooltip'
 import * as actions from '../actions';
 import { syncStatusPerCalendar } from '../getters';
+import ConnectionDetails from './ConnectionDetails';
 
 
 const mapStateToProps = (state) => ({
@@ -26,88 +20,6 @@ const mapStateToProps = (state) => ({
   connectionDetails: state.get('databaseDetails')
 });
 
-
-class Cleartext extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      justCopied: false
-    };
-  }
-
-  copy = () => {
-    const { text } = this.props;
-
-    navigator.clipboard.writeText(text);
-
-    this.setState({justCopied: true});
-
-    window.setTimeout(this.setState.bind(this, {justCopied: false}), 2000);
-  };
-
-  render() {
-    const { text } = this.props;
-    const { justCopied } = this.state;
-    let dataTip = !justCopied ? 'Click to copy' : 'Copied!';
-    if (!this.props.clickToCopy) {
-      dataTip = null;
-    }
-
-    return (
-        <span
-          key={justCopied ? 'copied' : 'copy' /* (forces re-render of tooltip) */}
-          data-tip={dataTip}
-          style={{cursor: 'pointer'}}
-          onClick={this.props.clickToCopy ? this.copy : () => {}}
-        >
-          {text}
-          <ReactTooltip effect="solid" />
-        </span>
-    )
-  }
-}
-
-
-class PasswordCell extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      showPassword: false,
-    };
-  }
-
-  togglePassword = () => {
-    this.setState({ showPassword: !this.state.showPassword });
-  };
-
-  render() {
-    const { password } = this.props;
-    const { showPassword } = this.state;
-
-    return (
-        <TableCell
-            align="right"
-            style={{display: 'flex', alignItems: 'center', 'fontFamily': '"Courier New", Courier, monospace'}}
-        >
-          {showPassword ?
-              <VisibilityOff
-                style={{marginLeft: '5px', cursor: 'pointer'}}
-                onClick={this.togglePassword}
-              />
-              : <Visibility
-                  style={{marginLeft: '5px', cursor: 'pointer'}}
-                  onClick={this.togglePassword}
-                />
-          }
-          {!showPassword ? <span>************</span> :
-            <Cleartext text={password} clickToCopy={true} />
-          }
-        </TableCell>
-    )
-  }
-}
 
 const SyncStarted = () => {
   return  (
@@ -140,73 +52,6 @@ const SyncStarted = () => {
                 </p>
               </div>
   )
-}
-
-const ConnectionDetails = ({ details }) => {
-  if (!details) return null;
-
-  const rows = [
-    {
-      label: 'Host',
-      value: details.get('host'),
-      clickToCopy: true
-    },
-    {
-      label: 'Port',
-      value: details.get('port'),
-      clickToCopy: true
-    },
-    {
-      label: 'Username',
-      value: details.get('username'),
-      clickToCopy: true
-    },
-    {
-      label: 'Password',
-      value: details.get('password'),
-      clickToCopy: true
-    },
-    {
-      label: 'Database name',
-      value: details.get('name'),
-      clickToCopy: true
-    },
-    {
-      label: 'Certificate Authority file for SSL',
-      value: (
-        <a href="https://s3.amazonaws.com/rds-downloads/rds-ca-2015-root.pem" target="_blank" rel="noopener noreferrer">
-          Download here
-        </a>
-      ),
-      clickToCopy: false
-    }
-  ];
-
-  return (
-      <div>
-        <h4>
-          MySQL connection details
-        </h4>
-        <Table aria-label="simple table">
-          <TableBody>
-            {rows.map(row => (
-                <TableRow key={row.label}>
-                  <TableCell component="th" scope="row">
-                    {row.label}
-                  </TableCell>
-                      {row.label !== 'Password' ?
-                      <TableCell align="right" style={{'fontFamily': '"Courier New", Courier, monospace'}}>
-
-                            <Cleartext text={row.value} clickToCopy={row.clickToCopy} />
-                      </TableCell>
-                      : <PasswordCell password={row.value}/>
-                      }
-                </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </div>
-  );
 };
 
 
@@ -326,7 +171,10 @@ class DatabaseDetails extends React.Component {
                 <CalendarsSelectorContainer />
             }
             <br />
-            <ConnectionDetails details={this.props.connectionDetails} />
+            <ConnectionDetails
+              details={this.props.connectionDetails}
+              hideHeader={true}
+            />
           </Container>
         </React.Fragment>
     );
